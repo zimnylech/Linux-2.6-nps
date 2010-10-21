12747993cb2b4a07df7a62f247f20aa4a6263cd9
N.P.S <napohybelskurwysynom2010@gmail.com>
2010-10-21 23:36:00 +0200

    Removal of dead code from arch/powerpc/kernel/process.c
    Signed-off-by: Zimny Lech <napohybelskurwysynom2010@gmail.com>
    CC: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    CC: Paul Mackerras <paulus@samba.org>
    CC: <linuxppc-dev@lists.ozlabs.org>
    CC: <linux-kernel@vger.kernel.org>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -670,12 +670,7 @@ void exit_thread(void)
 void flush_thread(void)
 {
 	discard_lazy_cpu_state();
-
-#ifdef CONFIG_HAVE_HW_BREAKPOINTS
-	flush_ptrace_hw_breakpoint(current);
-#else /* CONFIG_HAVE_HW_BREAKPOINTS */
 	set_debug_reg_defaults(&current->thread);
-#endif /* CONFIG_HAVE_HW_BREAKPOINTS */
 }
 
 void
